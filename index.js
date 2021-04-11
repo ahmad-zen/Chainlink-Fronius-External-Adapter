a5680e43dfe1853926a9f4a26de99c6c0d40147d
Scott Nguyen <scott.nguyen@zenecosystems.com>
2021-04-10 18:11:14 -0700

    completed external adapter to mock api

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 const { Requester, Validator } = require('@chainlink/external-adapter')
-
+// require('dotenv').config()
+// var apiKey = process.env.API_KEY
 // Define custom error scenarios for the API.
 // Return true for the adapter to retry.
 const customError = (data) => {
   if (data.Response === 'Error') return true
   return false
 }
-
 // Define custom parameters to be used by the adapter.
 // Extra parameters can be stated in the extra object,
 // with a Boolean value indicating whether or not they
 // should be required.
 const customParams = {
-  base: ['base', 'from', 'coin'],
-  quote: ['quote', 'to', 'market'],
+  DeviceId: false,
   endpoint: false
 }
-
 const createRequest = (input, callback) => {
   // The Validator helps you validate the Chainlink request data
   const validator = new Validator(callback, input, customParams)
   const jobRunID = validator.validated.id
-  const endpoint = validator.validated.data.endpoint || 'price'
-  const url = `https://min-api.cryptocompare.com/data/${endpoint}`
-  const fsym = validator.validated.data.base.toUpperCase()
-  const tsyms = validator.validated.data.quote.toUpperCase()
-
-  const params = {
-    fsym,
-    tsyms
+  const endpoint = validator.validated.data.endpoint || 'solar_api/v1/GetInverterRealtimeData.cgi'
+  const url = `https://sample-nodejs-lhygg.ondigitalocean.app/${endpoint}`
+  var DeviceId = validator.validated.data.DeviceId
+  const headers = {
+    DeviceId : `${DeviceId}`
   }
-
-  // This is where you would add method and headers
+  // This is where you would add method and headers      response.data.result = Requester.validateResultNumber(response.data, ['TOTAL_ENERGY', 'Value'])
   // you can add method like GET or POST and add it to the config
   // The default is GET requests
   // method = 'get' 
   // headers = 'headers.....'
-  const config = {
-    url,
-    params
+  var config
+  if (endpoint == 'provision') {
+    config = {
+      url
+    }
+  } else {
+      config = {
+      url,
+      headers
+    }
   }
-
   // The Requester allows API calls be retry in case of timeout
   // or connection failure
   Requester.request(config, customError)
@@ -48,14 +48,15 @@ const createRequest = (input, callback) => {
       // It's common practice to store the desired value at the top-level
       // result key. This allows different adapters to be compatible with
       // one another.
-      response.data.result = Requester.validateResultNumber(response.data, [tsyms])
+      if (DeviceId != null) {
+        response.data.result = Requester.validateResultNumber(response.data, ['TOTAL_ENERGY', 'Value'])
+      }
       callback(response.status, Requester.success(jobRunID, response))
     })
     .catch(error => {
       callback(500, Requester.errored(jobRunID, error))
     })
 }
-
 // This is a wrapper to allow the function to work with
 // GCP Functions
 exports.gcpservice = (req, res) => {
@@ -63,7 +64,6 @@ exports.gcpservice = (req, res) => {
     res.status(statusCode).send(data)
   })
 }
-
 // This is a wrapper to allow the function to work with
 // AWS Lambda
 exports.handler = (event, context, callback) => {
@@ -71,7 +71,6 @@ exports.handler = (event, context, callback) => {
     callback(null, data)
   })
 }
-
 // This is a wrapper to allow the function to work with
 // newer AWS Lambda implementations
 exports.handlerv2 = (event, context, callback) => {
@@ -83,7 +82,6 @@ exports.handlerv2 = (event, context, callback) => {
     })
   })
 }
-
 // This allows the function to be exported for testing
 // or for running in express
 module.exports.createRequest = createRequest
