d06259499284376fb3acae54bf11107116a004ba
Ahmad Alhayek <Ahmad.Alhayek@zenecosystems.com>
2021-04-10 23:42:36 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.EA_PORT || 8080
-const simulator = require('./Simulator');
-const http = require('http');
-const url = require('url');
+// const simulator = require('./Simulator');
+// const http = require('http');
+// const url = require('url');
 
 app.use(bodyParser.json())
 
@@ -20,27 +20,27 @@ app.post('/', (req, res) => {
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
 
-const simulatorPort = 3030;
-http.createServer((req, res) => {
-    const reqUrl = url.parse(req.url).pathname;
-    const deviceId = req.headers.deviceid;
+// const simulatorPort = 3030;
+// http.createServer((req, res) => {
+//     const reqUrl = url.parse(req.url).pathname;
+//     const deviceId = req.headers.deviceid;
 
-    if (reqUrl === "/solar_api/v1/GetInverterRealtimeData.cgi") {
-        res.writeHead(200, {
-            'Content-Type': "application/json"
-        });
-        const response = simulator.inverterCloudSimulator.getResponse(deviceId);
-        res.end(JSON.stringify(response));
-    }
-    if (reqUrl === "/provision") {
-        res.writeHead(200, {
-            'Content-Type': "application/json"
-        });
-        const response = simulator.inverterCloudSimulator.provisionDevice();
-        res.end(JSON.stringify(response));
-    }
-    res.writeHead(400, {
-        'Content-Type': "application/json"
-    });
-    res.end();
-}).listen(simulatorPort);
+//     if (reqUrl === "/solar_api/v1/GetInverterRealtimeData.cgi") {
+//         res.writeHead(200, {
+//             'Content-Type': "application/json"
+//         });
+//         const response = simulator.inverterCloudSimulator.getResponse(deviceId);
+//         res.end(JSON.stringify(response));
+//     }
+//     if (reqUrl === "/provision") {
+//         res.writeHead(200, {
+//             'Content-Type': "application/json"
+//         });
+//         const response = simulator.inverterCloudSimulator.provisionDevice();
+//         res.end(JSON.stringify(response));
+//     }
+//     res.writeHead(400, {
+//         'Content-Type': "application/json"
+//     });
+//     res.end();
+// }).listen(simulatorPort);
